368f73a3af13b9bc87d15d59904c32165ed8ebfd
govind <rknecbackup@gmail.com>
2021-07-01 16:43:42 +0530

    sdf

---
+++ src/main/java/controller/HelloController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	public HelloController() {
+		// TODO Auto-generated constructor stub
+	}
+	@GetMapping("/")
+	public String show() {
+		return "WELCOME DOCKER N";
+	}
+
+}
